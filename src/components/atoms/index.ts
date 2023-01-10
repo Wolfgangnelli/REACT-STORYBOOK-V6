32b9ca905062fefc57800bcc34d090072cd0135c
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2023-01-10 21:41:39 +0100

    story within story

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,16 @@
+import Button from './Button/Button'
+import { Primary, Secondary, Success, Danger } from './Button/Button.stories'
+import Input from './Input/Input'
+import { Large, Medium, Small } from './Input/Input.stories'
+
+export {
+    Button,
+    Input,
+    Primary,
+    Secondary,
+    Success,
+    Danger,
+    Large,
+    Medium,
+    Small,
+}
