45d7de3e64b2f0db241088e90644e2772c9e1ccc
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2023-01-11 15:37:05 +0100

    add args

--- src/components/atoms/ChakraButton/ChakraButton.stories.tsx
+++ src/components/atoms/ChakraButton/ChakraButton.stories.tsx
@@ -4,8 +4,24 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 export default {
     title: 'Chakra/Button',
-    component: Button
+    component: Button,
+    argTypes: {
+        colorScheme: { control: 'text' },
+        children: { control: 'tet' },
+        onClick: { action: 'clicked' },
+    }
 } as ComponentMeta<typeof Button>
 
-export const Success: ComponentStory<typeof Button> = () => <Button colorScheme='green'>Success</Button>
-export const Danger: ComponentStory<typeof Button> = () => <Button colorScheme='red'>Success</Button>
+const Template = (args: any) => <Button {...args} />
+
+export const Success: ComponentStory<typeof Button> = Template.bind({})
+Success.args = {
+    colorScheme: 'green',
+    children: 'Success'
+}
+
+export const Danger: ComponentStory<typeof Button> = Template.bind({})
+Danger.args = {
+    colorScheme: 'red',
+    children: 'Danger'
+}
